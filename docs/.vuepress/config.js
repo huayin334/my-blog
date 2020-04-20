9d7b39b3f650f5ffdf145eb454128892046a465a
lijuan <761916205@qq.com>
2020-04-20 16:08:17 +0800

    [add]第一次

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  "title": "blog",
+  "description": "",
+  "base":"/dist/",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "NPM",
+            "link": "https://www.npmjs.com/~reco_luan",
+            "icon": "reco-npm"
+          },
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          },
+          {
+            "text": "简书",
+            "link": "https://www.jianshu.com/u/cd674a19515e",
+            "icon": "reco-jianshu"
+          },
+          {
+            "text": "CSDN",
+            "link": "https://blog.csdn.net/recoluan",
+            "icon": "reco-csdn"
+          },
+          {
+            "text": "博客圆",
+            "link": "https://www.cnblogs.com/luanhewei/",
+            "icon": "reco-bokeyuan"
+          },
+          {
+            "text": "WeChat",
+            "link": "https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A",
+            "icon": "reco-wechat"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "huayin",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
